d0c3839eb10d9e9f23585946fcd28d80a6bd8dbc
Sus <a20250507@utem.edu.mx>
2026-09-23 23:01:24 -0600

    Agrego pantalla de inicio

--- app/src/main/java/com/gastoseguro/miapp/MainActivity.kt
+++ app/src/main/java/com/gastoseguro/miapp/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Gasto $name!",
         modifier = modifier
     )
 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     GastoSeguroTheme {
-        Greeting("Android")
+        Greeting("Seguro")
     }
 }
